20c38895eaa152333d58f4330110b10edb91f69b
CatherineP37 <>
2024-01-25 05:24:14 +0000

    Create Django app

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from application.views import testing
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('appointments/', testing, name='appointments'),
 ]
