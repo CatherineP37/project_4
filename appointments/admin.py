33d61599c9b8651806fba1dcfd30a00e10e03f15
CatherineP37 <>
2024-01-26 15:07:10 +0000

    Add Doctor_5_availability model to models.py

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Patients, Appointments, Doctor_1_availability, Doctor_2_availability, Doctor_3_availability, Doctor_4_availability
+from .models import Patients, Appointments, Doctor_1_availability, Doctor_2_availability, Doctor_3_availability, Doctor_4_availability, Doctor_5_availability
 
 # Register your models here.
 admin.site.register(Patients)
@@ -8,3 +8,4 @@ admin.site.register(Doctor_1_availability)
 admin.site.register(Doctor_2_availability)
 admin.site.register(Doctor_3_availability)
 admin.site.register(Doctor_4_availability)
+admin.site.register(Doctor_5_availability)
--- appointments/models.py
+++ appointments/models.py
@@ -44,3 +44,9 @@ class Doctor_4_availability(models.Model):
     appointment_id = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
+
+
+class Doctor_5_availability(models.Model):
+    appointment_id = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
