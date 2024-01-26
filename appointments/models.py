22d14fc5ec50d1b81eea8dd39d7136dd305ba5ce
CatherineP37 <>
2024-01-26 12:12:14 +0000

    Add the Patients model to models.py

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here
+
+
+class Patients(models.Model):
+    patient_id_number = models.IntegerField()
+    first_name = models.CharField()
+    last_name = models.CharField()
+    email_address = models.EmailField()
+    postal_address = models.CharField()
+    phone_number = models.IntegerField()
