6dcb63405dc0318f11228d17cf6fb7e3d0121015
CatherineP37 <>
2024-02-08 11:12:07 +0000

    Add SECRET_KEY

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q03m)&w_^98mezrgt)h!xxta6@19^m$1$tk76$wot#+87=ezlt'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
