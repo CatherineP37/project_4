31667c33925b15aa6de941e44756607450b34265
CatherineP37 <>
2024-02-08 11:56:41 +0000

    Set DEBUG to True

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-catherinep37-project-4-m4w951cu2q.us2.codeanyapp.com', 'doctor-appointment-booker.herokuapp.com']
