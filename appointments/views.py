1ae74b3da01ca8dfc18962c45f2e5d68c11bfd0a
CatherineP37 <>
2024-02-09 12:28:32 +0000

    Add views for rest of pages

--- appointments/views.py
+++ appointments/views.py
@@ -8,3 +8,48 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html', {})
 
+def account(request):
+    return render(request, 'account.html', {})
+
+def appointments(request):
+    return render(request, 'appointments.html', {})
+
+def booking(request):
+    return render(request, 'booking.html', {})
+
+def cancellation(request):
+    return render(request, 'cancellation.html', {})
+
+def change_password(request):
+    return render(request, 'change_password.html', {})
+
+def close_account(request):
+    return render(request, 'close_account.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+def doctor_1(request):
+    return render(request, 'doctor_1.html', {})
+
+def doctors(request):
+    return render(request, 'doctors.html', {})
+
+def email_sent(request):
+    return render(request, 'email_sent.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
+def lost_password(request):
+    return render(request, 'lost_password.html', {})
+
+def past_appointments(request):
+    return render(request, 'past_appointments.html', {})
+
+def sign_up(request):
+    return render(request, 'sign_up.html', {})
+
+
+
+
