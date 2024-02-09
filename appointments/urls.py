679b891299070c91da47953eb644ce9be2b62fef
CatherineP37 <>
2024-02-09 12:07:30 +0000

    Add urls for the other pages in urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,4 +3,19 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('account.html', views.account, name="account"),
+    path('appointments.html', views.appointments, name="appointments"),
+    path('booking.html', views.booking, name="booking"),
+    path('cancellation.html', views.cancellation, name="cancellation"),
+    path('change_password.html', views.change_password, name="change_password"),
+    path('close_account.html', views.close_account, name="close_account"),
+    path('contact.html', views.contact, name="contact"),
+    path('doctor_1.html', views.doctor_1, name="doctor_1"),
+    path('doctors.html', views.doctors, name="doctors"),
+    path('email_sent.html', views.email_sent, name="email_sent"),
+    path('login.html', views.login, name="login"),
+    path('past_appointments.html', views.past_appointments, name="past_appointments"),
+    path('sign_up.html', views.sign_up, name="sign_up"),
+
+
 ]
