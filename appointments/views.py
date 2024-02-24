800ec5cf1bf69d98631d13ebb31a961f350d7845
Catherine Parrish <cathp27@gmail.com>
2024-02-24 13:27:50 +0000

    Correct typo

--- appointments/views.py
+++ appointments/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import doctor_1_availability
+from .models import Doctor_1_availability
 from django.http import HttpResponse
 
 # Create your views here.
@@ -25,8 +25,8 @@ def close_account(request):
     return render(request, 'close_account.html', {})
 
 def doctor_1(request):
-    available_appointments = doctor_1_availability.objects.all
-    return render(request, 'doctor_1.html', {'all':available_appointments})
+    available_appointments = Doctor_1_availability.objects.all
+    return render(request, 'doctor_1.html', {'available_appointments':available_appointments})
 
 def doctors(request):
     return render(request, 'doctors.html', {})
