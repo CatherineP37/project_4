0a39760a79ba042944b8788994c8e0c30a3d7855
Catherine Parrish <cathp27@gmail.com>
2024-02-28 17:40:40 +0000

    Edit import on admin.py

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Patient, Appointment, Doctor_1_availability, Doctor_2_availability, Doctor_3_availability, Doctor_4_availability, Doctor_5_availability, Doctor_6_availability
+from .models import Booked_appointments, Availability
 
 # Register your models here.
 admin.site.register(Booked_appointments)
