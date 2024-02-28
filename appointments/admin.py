c0a58440481df1993eb6ac713f267ab67d0676e9
Catherine Parrish <cathp27@gmail.com>
2024-02-28 17:39:15 +0000

    Edit admin.py

--- appointments/admin.py
+++ appointments/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import Patient, Appointment, Doctor_1_availability, Doctor_2_availability, Doctor_3_availability, Doctor_4_availability, Doctor_5_availability, Doctor_6_availability
 
 # Register your models here.
-admin.site.register(Patient)
-admin.site.register(Appointment)
-admin.site.register(Doctor_1_availability)
-admin.site.register(Doctor_2_availability)
-admin.site.register(Doctor_3_availability)
-admin.site.register(Doctor_4_availability)
-admin.site.register(Doctor_5_availability)
-admin.site.register(Doctor_6_availability)
+admin.site.register(Booked_appointments)
+admin.site.register(Availability)
+
