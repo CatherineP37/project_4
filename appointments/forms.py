a593a81d0d6afae7f305a56b5598920870288119
Catherine Parrish <cathp27@gmail.com>
2024-02-29 07:12:31 +0000

    Edit BookAppointment form

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Availability
+from .models import Booked_appointments
 
 class BookAppointment(ModelForm):  
     class Meta:
-        model = Availability
-        fields = ('date', 'time')
+        model = Booked_appointments
+        fields = ('first_name', 'last_name', 'email_address', 'phone_number')
