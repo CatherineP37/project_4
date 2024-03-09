025658f5badbc86f13dbfb6980488435a2343948
Catherine Parrish <cathp27@gmail.com>
2024-03-09 11:52:23 +0000

    Comment out some code

--- appointments/urls.py
+++ appointments/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('cancellation/<str:pk>/', views.cancellation, name="cancellation"),   
     path('close_account/', views.close_account, name="close_account"),  
     path('booked_appointment/', views.booked_appointment, name="booked_appointment"),
-    path('double_booked/', views.double_booked, name="double_booked"),
+   # path('double_booked/', views.double_booked, name="double_booked"),
 ]
