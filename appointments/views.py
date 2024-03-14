e2445c3cf02a4dc13eb0b26056c6f62372fd237c
Catherine Parrish <cathp27@gmail.com>
2024-03-14 15:53:20 +0000

    Delete delete_appointment view

--- appointments/views.py
+++ appointments/views.py
@@ -67,24 +67,6 @@ def booked_appointment(request):
 def double_booked(request):
     return render(request, 'double_booked.html', {})
 
-def delete_appointment(request, pk):
-   
-    appointment_instance = get_object_or_404(Booked_appointments, id=pk)
-
-    if appointment_instance.is_past_date:
-        action = 'delete record of'
-    else:
-        action = 'cancel'
-
-        context = {
-            'appointment_instance': appointment_instance,
-            'action': action,
-        }
-        return render(request, 'delete_appointment.html', context)
-
-
-
-
 def update_booking(request, pk):
     appointment = get_object_or_404(Booked_appointments, id=pk)
     if request.method == 'POST':
