e1c58805ebf933dd3ce7ccb284c480ee3d6fb843
Catherine Parrish <cathp27@gmail.com>
2024-03-17 12:54:55 +0000

    Remove white space from urls.py

--- appointments/urls.py
+++ appointments/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('account/',views.account, name="account"), 
+    path('account/',views.account, name="account"),
     path('booking/', views.booking, name="booking"),
     path('cancellation/<str:pk>/', views.cancellation, name="cancellation"),
     path('booked_appointment/', views.booked_appointment, name="booked_appointment"),
-    path('update_booking/<str:pk>', views.update_booking, name="update_booking"), 
+    path('update_booking/<str:pk>', views.update_booking, name="update_booking"),
 ]
