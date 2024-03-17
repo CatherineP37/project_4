2c3c692bd7031903d1eb1a102a7819d00c2a8bed
Catherine Parrish <cathp27@gmail.com>
2024-03-17 12:59:34 +0000

    Edit apps.py

--- appointments/forms.py
+++ appointments/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from .models import Booked_appointments
 
+
 class BookAppointment(ModelForm):
     class Meta:
         model = Booked_appointments
